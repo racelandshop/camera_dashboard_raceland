f126c4621265a3daa5ab88857fa7796246028e9c
rogerio <zroger499@gmail.com>
2023-06-09 17:22:53 +0100

    Finished rework

--- custom_components/alarmo/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""Custom Exceptions for HACS."""
-
-
-class CameraExecutionStillInProgress(Exception):
-    """Super basic."""
-
-class CameraExecutionStillInProgress(CameraExecutionStillInProgress):
-    """Exception to raise if execution is still in progress."""
-
--- custom_components/alarmo/frontend/src/data/dashboard.ts
+++
@@ -1,8 +0,0 @@
-export interface Dashboard {
-  language: string;
-  updates: any[];
-  resources: any[];
-  removed: any[];
-  sections: any;
-  localize(string: string, replace?: Record<string, any>): string;
-}
